778e3d6193ed5fdfec115ca8e74b388cbbf93b08
Antoine-Goncalves-ISC-France <antoine.goncalves-isc.france@ibm.com>
2021-09-27 11:41:11 +0200

    add chai and cross-env

--- test/test.js
+++ test/test.js
@@ -2,6 +2,22 @@ const assert = require('assert');
 
 describe('file to be tested', () => {
   context('function to be tested', () => {
+    // before(() => {
+    //   console.log('=====before');
+    // });
+
+    // after(() => {
+    //   console.log('=====after');
+    // });
+
+    // beforeEach(() => {
+    //   console.log('=====beforeEach');
+    // });
+
+    // afterEach(() => {
+    //   console.log('=====afterEach');
+    // });
+
     it('should do something', () => {
       assert.equal(1, 1);
     });
@@ -11,5 +27,9 @@ describe('file to be tested', () => {
     });
 
     it('this is a peding test');
+  });
+
+  context('another function', () => {
+    it('should do something');
   });
 });
